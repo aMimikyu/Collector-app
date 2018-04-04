0d37d3eb0e231653c8edd9e9575ad323c41f8cba
Why is this so hard to use? <ael4evernikos@yahoo.gr>
2018-04-04 17:05:11 +0300

    Update CREATE TABLES.sql

--- SQL/CREATE TABLES.sql
+++ SQL/CREATE TABLES.sql
@@ -25,46 +25,46 @@ CREATE TABLE Collections ( Collection_ID INT UNSIGNED AUTO_INCREMENT ,
 
 
 CREATE TABLE Collectors ( Username VARCHAR(30),
-	  					  Collection_ID INT UNSIGNED,
-	  					  Participation_Date DATETIME NOT NULL,
-	  					  FOREIGN KEY collectors_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE ,               					 
-	  					  FOREIGN KEY collectors_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,  	 
-	  					  PRIMARY KEY collectors_usrnm_colID_pk (Username,Collection_ID));
+	  		  Collection_ID INT UNSIGNED,
+	  		  Participation_Date DATETIME NOT NULL,
+	  		  FOREIGN KEY collectors_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE ,               					 
+	  		  FOREIGN KEY collectors_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,  	 
+	  		  PRIMARY KEY collectors_usrnm_colID_pk (Username,Collection_ID));
 
 
 
 CREATE TABLE Liked_Collections ( Username VARCHAR(30),
-								Collection_ID INT UNSIGNED,
-								FOREIGN KEY liked_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE ,
-								FOREIGN KEY liked_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
-								PRIMARY KEY liked_usrnm_colID_pk (Username,Collection_ID));
+				 Collection_ID INT UNSIGNED,
+				 FOREIGN KEY liked_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE ,
+				 FOREIGN KEY liked_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
+				 PRIMARY KEY liked_usrnm_colID_pk (Username,Collection_ID));
 
 
 
 CREATE TABLE Collection_Comments ( Comment_ID  INT AUTO_INCREMENT, 
-								  Username VARCHAR(30) NOT NULL,
-								  Collection_ID INT UNSIGNED NOT NULL,
-								  Comment TEXT NOT NULL,
-								  Comment_Date DATETIME NOT NULL,
-								  FOREIGN KEY col_comm_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE,
-								  FOREIGN KEY col_comm_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
-								  PRIMARY KEY col_comm_comID_pk (Comment_ID));
+				   Username VARCHAR(30) NOT NULL,
+				   Collection_ID INT UNSIGNED NOT NULL,
+				   Comment TEXT NOT NULL,
+				   Comment_Date DATETIME NOT NULL,
+				   FOREIGN KEY col_comm_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE,
+				   FOREIGN KEY col_comm_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
+				   PRIMARY KEY col_comm_comID_pk (Comment_ID));
 
 
 
 CREATE TABLE Collection_Items ( Collection_ID INT UNSIGNED,
-								Item VARCHAR(255),
-								FOREIGN KEY col_itms_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
-								PRIMARY KEY col_itms_itm_colID_pk (Collection_ID,Item));
+			        Item VARCHAR(255),
+				FOREIGN KEY col_itms_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
+				PRIMARY KEY col_itms_itm_colID_pk (Collection_ID,Item));
 
 
 
 CREATE TABLE Items_Owned ( Username VARCHAR(30),
-						  Collection_ID INT UNSIGNED,
-						  Item VARCHAR(255),
-						  FOREIGN KEY itms_ownd_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE,
-						  FOREIGN KEY itms_ownd_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
-						  PRIMARY KEY itms_ownd_usrnm_colID_itm_pk (Username,Collection_ID,Item));
+			Collection_ID INT UNSIGNED,
+			Item VARCHAR(255),
+			FOREIGN KEY itms_ownd_usrnm_fk (Username) REFERENCES Users(Username) ON DELETE CASCADE,
+			FOREIGN KEY itms_ownd_colID_fk (Collection_ID) REFERENCES Collections(Collection_ID) ON DELETE CASCADE,
+			PRIMARY KEY itms_ownd_usrnm_colID_itm_pk (Username,Collection_ID,Item));
 
 
 
